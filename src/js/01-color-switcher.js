8b84ff8a0f23c0bc9dfa012bed0dad61f4919aba
Iryna Hordiichuk <mason.iryna2@gmail.com>
2022-07-31 12:55:23 +0100

    color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', onStartHandler);
+stopBtn.addEventListener('click', onStopHandler);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  let myInterval;
+
+function onStartHandler (){
+    myInterval = setInterval(() => { document.body.style.backgroundColor = getRandomHexColor()},
+      1000); 
+    
+    stopBtn.removeAttribute("disabled");
+    startBtn.setAttribute("disabled", true);
+}
+
+function onStopHandler (){
+    clearInterval(myInterval);
+    startBtn.removeAttribute("disabled");
+    stopBtn.setAttribute("disabled", true);
+};
+
+
